b1bfc57b83377975e302c24522696803b2eb0956
shadowfax6894 <anniekelly6894@gmail.com>
2018-02-05 13:56:34 -0700

    changing video url

--- sim/video.ts
+++ sim/video.ts
@@ -14,7 +14,7 @@ namespace pxsim.video {
             player = new YT.Player('video-placeholder', {
                 width: w - widthOffset,
                 height: h - heightOffset,
-                videoId: '6v2L2UGZJAM',
+                videoId: '9p_Si21ig7c',
                 playerVars: {
                     color: 'white',
                 },
